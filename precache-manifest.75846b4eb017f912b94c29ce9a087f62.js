530be71a1d4455b4b204c56a03c62158cffcf0bd
pratikneupane <pratikneupane1999@gmail.com>
2020-04-15 21:16:36 +0545

    Updates

---
+++ precache-manifest.75846b4eb017f912b94c29ce9a087f62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e263b117102868a26e9b8105a8f43b5d",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "468dbe949647ef37ade4",
+    "url": "/react-todo-list/static/css/main.fe49db23.chunk.css"
+  },
+  {
+    "revision": "a46cc2fd1b6449dea63d",
+    "url": "/react-todo-list/static/js/2.cbd12496.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-list/static/js/2.cbd12496.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "468dbe949647ef37ade4",
+    "url": "/react-todo-list/static/js/main.0172d5c4.chunk.js"
+  },
+  {
+    "revision": "24763f4ac37c4c84a6e5",
+    "url": "/react-todo-list/static/js/runtime-main.b105f84a.js"
+  }
+]);
